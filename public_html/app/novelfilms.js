feca91dad2f752e775eb11638757fb27e1b72e2c
Craig <grahamcr.gvsu@gmail.com>
2017-04-20 10:26:57 -0400

    index update

---
+++ public_html/app/novelfilms.js
@@ -0,0 +1,16 @@
+
+'use strict';
+
+angular.module('novelfilms', [
+    'ngResource',
+    'novelfilms.home',
+    'novelfilms.contact',
+    'novelfilms.about'
+    
+]).config(['$httpProvider', function($httpProvider) {
+  $httpProvider.defaults.withCredentials = true;
+}]);
+
+
+
+                
