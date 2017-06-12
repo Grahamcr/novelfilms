f77392b73fef9506f2e1437e96d7b848274073b5
Craig <grahamcr.gvsu@gmail.com>
2017-06-12 10:41:22 -0400

    watch page redesign

--- public_html/app/watch/watchController.js
+++ public_html/app/watch/watchController.js
@@ -58,13 +58,48 @@
             }
       };
       
-      
+      $scope.gofullscreen = function(id) {
+          
+      };
     
     /***************************************************************************************
     * On Page Load Function
     ****************************************************************************************/   
 		angular.element(document).ready(function () {
-            
+           
+            $('.nf-carousel').slick({
+                dots: true,
+              infinite: false,
+              speed: 300,
+              slidesToShow: 4,
+              slidesToScroll: 4,
+              responsive: [
+                {
+                  breakpoint: 1024,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                  }
+                },
+                {
+                  breakpoint: 600,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                  }
+                },
+                {
+                  breakpoint: 480,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }
+
+              ]
+            });
             
             $(".preview-video").hover(function() {
               $(this).prop("controls", true);
--- public_html/app/watch/watchDirective.js
+++ public_html/app/watch/watchDirective.js
@@ -6,6 +6,39 @@
     
     
     
+     /***************************************************************************************
+    * Open the video in full screen
+    ****************************************************************************************/ 
+    .directive("openVideo", ['$location', function($location){
+        return function(scope, element, attrs){
+            element.bind("click", function(){
+
+                $('#'+attrs.vname).css('display', 'flex');
+                $('.header').hide();
+                $('body').css("overflow", "hidden");
+                $('html').css("overflow", "hidden");
+
+            });
+        };
+    }])
+    
+    
+     /***************************************************************************************
+    * Close video overlay
+    ****************************************************************************************/ 
+    .directive("closeVideo", ['$location', function($location){
+        return function(scope, element, attrs){
+            element.bind("click", function(){
+
+                $('.video-overlay').hide();
+                $('.header').show();
+                $('body').css("overflow-y", "auto");
+                $('html').css("overflow-y", "auto");
+                
+            });
+        };
+    }])
+    
 
     
         ;
